95c9a2d05380eb87df941d1619480fd1a51fd525
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-12-13 12:32:04 -0500

    a

--- app/src/main.js
+++ app/src/main.js
@@ -1,4 +1,16 @@
 import './style.css'
 
+let rollHistory = []
+function rollDice1(){
 let random = Math.floor(Math.random() * 6) + 1;
-console.log(random)
+console.log(random);
+rollHistory.push(random)
+}
+function rollDice2(){
+  let random2 = Math.floor(Math.random() * 6) + 1;
+  console.log(random2);
+  rollHistory.push(random2)
+}
+rollDice1()
+rollDice1()
+console.log(rollHistory)
