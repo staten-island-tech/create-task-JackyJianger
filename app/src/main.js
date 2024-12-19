db8907c7cfb426f985911ef8994e4c0db7a22cc6
JackyJianger <158211567+JackyJianger@users.noreply.github.com>
2024-12-19 12:32:13 -0500

    a

--- app/src/main.js
+++ app/src/main.js
@@ -2,6 +2,7 @@ import './style.css'
 
 const DOMSelectors = {
   containerBox: document.getElementById("container"),
+  containerBox2: document.getElementById("container2"),
 };
 
 let a=[];
@@ -12,12 +13,23 @@ let random = Math.floor(Math.random() * 6) + 1;
 a.push(random)
 console.log(a)
 }
-function rollDice2(){
+function rollDice2P1(){
   let random2 = Math.floor(Math.random() * 6) + 1;
+  let random3 = Math.floor(Math.random() * 6) + 1;
   a.push(random2)
+  a.push(random3)
+  console.log(a)
 }
-console.log(a)
 
+function rollDice3P1(){
+  let random1 = Math.floor(Math.random() * 6) + 1;
+  let random2 = Math.floor(Math.random() * 6) + 1;
+  let random3 = Math.floor(Math.random() * 6) + 1;
+  a.push(random1)
+  a.push(random2)
+  a.push(random3)
+  console.log(a)
+}
 
 function rollDice1P2(){
 let random = Math.floor(Math.random() * 6) + 1;
@@ -25,8 +37,24 @@ b.push(random)
 console.log(b)
 }
 
-
-
+function rollDice2P2(){
+  let random = Math.floor(Math.random() * 6) + 1;
+  let random2 = Math.floor(Math.random() * 6) + 1;
+  b.push(random)
+  b.push(random2)
+  console.log(b)
+  }
+  
+function rollDice3P2(){
+  let random = Math.floor(Math.random() * 6) + 1;
+  let random2 = Math.floor(Math.random() * 6) + 1;
+  let random3 = Math.floor(Math.random() * 6) + 1;
+  b.push(random)
+  b.push(random2)
+  b.push(random3)
+  console.log(b)
+  }
+  
 function compareDiceSums(a){
   let asum = 0
   let bsum = 0
@@ -54,3 +82,33 @@ if(event.target && event.target.classList.contains("roll-1")){
   rollDice1P1()
 }
 });
+
+DOMSelectors.containerBox.addEventListener("click", async function (event) {
+  if(event.target && event.target.classList.contains("roll-2")){
+    rollDice2P1()
+  }
+  });
+
+DOMSelectors.containerBox.addEventListener("click", async function (event) {
+  if(event.target && event.target.classList.contains("roll-3")){
+    rollDice3P1()
+  }
+  });
+
+DOMSelectors.containerBox2.addEventListener("click", async function (event) {
+  if(event.target && event.target.classList.contains("roll-1-2")){
+    rollDice1P2()
+  }
+  });
+    
+DOMSelectors.containerBox2.addEventListener("click", async function (event) {
+  if(event.target && event.target.classList.contains("roll-2-2")){
+    rollDice2P2()
+  }
+  });
+
+DOMSelectors.containerBox2.addEventListener("click", async function (event) {
+  if(event.target && event.target.classList.contains("roll-3-2")){
+    rollDice3P2()
+  }
+  });
